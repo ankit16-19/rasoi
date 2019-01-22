c8185d2856d36197d69d9e9f79c94c0ecbac59d3
unknown <ankitsocial16@gmail.com>
2019-01-22 20:25:25 +0530

    Updated PDF.go

--- PDF.go
+++ PDF.go
@@ -1,69 +1,71 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
 	"strconv"
 	"time"
 
+	. "github.com/ankit16-19/rasoi/models"
 	"github.com/jung-kurt/gofpdf"
-	"gopkg.in/mgo.v2/bson"
 )
 
-// Coupon structure
-type Coupon struct {
-	ID           bson.ObjectId `bson:"_id" json:"id"`
-	Userid       string        `bson:"userid" json:"userid"`
-	Gender       string        `bson:"gender" json:"gender"`
-	Amount1      int           `bson:"amount1" json:"amount1"`
-	Amount2      int           `bson:"amount2" json:"mount2"`
-	Total        int           `bson:"Total" json:"Total"`
-	WeekStartDay time.Time     `bson:"weekstartdate" json:"weekstartdate"`
-	Coupon       CouponForWeek `bson:"coupon" json:"coupon"`
-}
+// // Coupon structure
+// type Coupon struct {
+// 	ID           bson.ObjectId `bson:"_id" json:"id"`
+// 	Userid       string        `bson:"userid" json:"userid"`
+// 	Gender       string        `bson:"gender" json:"gender"`
+// 	Amount1      int           `bson:"amount1" json:"amount1"`
+// 	Amount2      int           `bson:"amount2" json:"mount2"`
+// 	Total        int           `bson:"Total" json:"Total"`
+// 	WeekStartDay time.Time     `bson:"weekstartdate" json:"weekstartdate"`
+// 	Coupon       CouponForWeek `bson:"coupon" json:"coupon"`
+// }
 
-// CouponForWeek : Coupon structure for whole week
-type CouponForWeek struct {
-	Mon CouponForDay `bson:"mon" json:"mon"`
-	Tue CouponForDay `bson:"tue" json:"tue"`
-	Wed CouponForDay `bson:"wed" json:"wed"`
-	Thr CouponForDay `bson:"thr" json:"thr"`
-	Fri CouponForDay `bson:"fri" json:"fri"`
-	Sat CouponForDay `bson:"sat" json:"sat"`
-	Sun CouponForDay `bson:"sun" json:"sun"`
-}
+// // CouponForWeek : Coupon structure for whole week
+// type CouponForWeek struct {
+// 	Mon CouponForDay `bson:"mon" json:"mon"`
+// 	Tue CouponForDay `bson:"tue" json:"tue"`
+// 	Wed CouponForDay `bson:"wed" json:"wed"`
+// 	Thr CouponForDay `bson:"thr" json:"thr"`
+// 	Fri CouponForDay `bson:"fri" json:"fri"`
+// 	Sat CouponForDay `bson:"sat" json:"sat"`
+// 	Sun CouponForDay `bson:"sun" json:"sun"`
+// }
 
-// CouponForDay : Coupon structure for a day
-type CouponForDay struct {
-	Breakfast FoodType `bson:"breakfast" json:"breakfast"`
-	Lunch     FoodType `bson:"lunch" json:"lunch"`
-	Dinner    FoodType `bson:"dinner" json:"dinner"`
-}
+// // CouponForDay : Coupon structure for a day
+// type CouponForDay struct {
+// 	Breakfast FoodType `bson:"breakfast" json:"breakfast"`
+// 	Lunch     FoodType `bson:"lunch" json:"lunch"`
+// 	Dinner    FoodType `bson:"dinner" json:"dinner"`
+// }
 
-// FoodType : FoodStructure for a time
-type FoodType struct {
-	IsSelected bool `bson:"isSelected" json:"isSelected"`
-	IsVeg      bool `bson:"isVeg" json:"isVeg"`
-	IsMessup   bool `bson:"ismessup" json:"isMessUp"`
-}
+// // FoodType : FoodStructure for a time
+// type FoodType struct {
+// 	IsSelected bool `bson:"isSelected" json:"isSelected"`
+// 	IsVeg      bool `bson:"isVeg" json:"isVeg"`
+// 	IsMessup   bool `bson:"ismessup" json:"isMessUp"`
+// }
 
-type studentCouponInfo struct {
+// StudentCouponInfo :
+type StudentCouponInfo struct {
 	UserID string
 	Gender string
 	Total  int
 }
 
-type totalCouponCount struct {
-	Mon totalFoodForDay
-	Tue totalFoodForDay
-	Wed totalFoodForDay
-	Thr totalFoodForDay
-	Fri totalFoodForDay
-	Sat totalFoodForDay
-	Sun totalFoodForDay
+// TotalCouponCount :
+type TotalCouponCount struct {
+	Mon TotalFoodForDay
+	Tue TotalFoodForDay
+	Wed TotalFoodForDay
+	Thr TotalFoodForDay
+	Fri TotalFoodForDay
+	Sat TotalFoodForDay
+	Sun TotalFoodForDay
 }
 
-type totalFoodForDay struct {
+// TotalFoodForDay :
+type TotalFoodForDay struct {
 	BVeg  int
 	BNVeg int
 	LVeg  int
@@ -72,34 +74,34 @@ type totalFoodForDay struct {
 	DNVeg int
 }
 
-func main() {
-	// c := [10]Coupon{}
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 10)
+// func main() {
 
-	// for i := 0; i < 10; i++ {
-	// 	if i%2 == 0 {
-	// 		PrintCouponToPDF(c[i], pdf, "Mess-1", true)
-	// 	} else {
-	// 		PrintCouponToPDF(c[i], pdf, "Mess-1", false)
+// 	pdf := gofpdf.New("P", "mm", "A4", "")
+// 	pdf.AddPage()
+// 	pdf.SetFont("Arial", "B", 10)
 
-	// 	}
-	// }
+// 	// c := [10]Coupon{}
+// 	// for i := 0; i < 10; i++ {
+// 	// 	if i%2 == 0 {
+// 	// 		PrintCouponToPDF(c[i], pdf, "Mess-1", true)
+// 	// 	} else {
+// 	// 		PrintCouponToPDF(c[i], pdf, "Mess-1", false)
+// 	// 	}
+// 	// }
 
-	// studentinfo := [10]studentCouponInfo{}
-	// for i, si := range studentinfo {
-	// 	PrintStudentCouponInfoToPdf(i, si, pdf, "Mess-1")
-	// }
+// 	// studentinfo := [10]StudentCouponInfo{}
+// 	// for i, si := range studentinfo {
+// 	// 	PrintStudentCouponInfoToPdf(i, si, pdf, "Mess-1")
+// 	// }
 
-	err := pdf.OutputFileAndClose("hello.pdf")
-	if err != nil {
-		fmt.Print("err", err)
-	}
-}
+// 	err := pdf.OutputFileAndClose("hello.pdf")
+// 	if err != nil {
+// 		fmt.Print("err", err)
+// 	}
+// }
 
 // PrintStudentCouponInfoToPdf :
-func PrintStudentCouponInfoToPdf(srl int, s studentCouponInfo, p *gofpdf.Fpdf, mess string) {
+func PrintStudentCouponInfoToPdf(srl int, s StudentCouponInfo, p *gofpdf.Fpdf, mess string) {
 	var l, h float64
 	l = 39
 	h = 10
@@ -122,7 +124,7 @@ func PrintStudentCouponInfoToPdf(srl int, s studentCouponInfo, p *gofpdf.Fpdf, m
 }
 
 // PrintTotalCountToPDF :
-func PrintTotalCountToPDF(t totalCouponCount, p *gofpdf.Fpdf, mess string) {
+func PrintTotalCountToPDF(t TotalCouponCount, p *gofpdf.Fpdf, mess string) {
 	var l, h float64
 	l = 25
 	h = 10
@@ -239,29 +241,29 @@ func printCell(p *gofpdf.Fpdf, l float64, h float64, txt string) {
 	p.CellFormat(l, h, txt, fullBorder, contSameLine, middleAlign, noColor, noLink, emptyLink)
 }
 
-// GetDateFromTime :
-func GetDateFromTime(t time.Time) string {
-	return t.Format(time.RFC3339)[:10]
-}
+// // GetDateFromTime :
+// func GetDateFromTime(t time.Time) string {
+// 	return t.Format(time.RFC3339)[:10]
+// }
 
-// FirstDayofWeek :
-func FirstDayofWeek(t time.Time) time.Time {
-	for t.Weekday() != time.Monday {
-		t = t.AddDate(0, 0, -1)
-	}
-	return t
-}
+// // FirstDayofWeek :
+// func FirstDayofWeek(t time.Time) time.Time {
+// 	for t.Weekday() != time.Monday {
+// 		t = t.AddDate(0, 0, -1)
+// 	}
+// 	return t
+// }
 
-// WholeWeekDates :
-func WholeWeekDates(t time.Time) []time.Time {
-	var array []time.Time
-	t = FirstDayofWeek(t)
-	// // Add 7 days to get next week monday date
-	// t = t.AddDate(0, 0, 7)
-	array = append(array, t)
-	for t.Weekday() != time.Sunday {
-		t = t.AddDate(0, 0, 1)
-		array = append(array, t)
-	}
-	return array
-}
+// // WholeWeekDates :
+// func WholeWeekDates(t time.Time) []time.Time {
+// 	var array []time.Time
+// 	t = FirstDayofWeek(t)
+// 	// // Add 7 days to get next week monday date
+// 	// t = t.AddDate(0, 0, 7)
+// 	array = append(array, t)
+// 	for t.Weekday() != time.Sunday {
+// 		t = t.AddDate(0, 0, 1)
+// 		array = append(array, t)
+// 	}
+// 	return array
+// }
