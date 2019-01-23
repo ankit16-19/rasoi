04e5a23cdbb2c4eb0df114b0c86117a3a1252566
unknown <ankitsocial16@gmail.com>
2019-01-23 11:06:56 +0530

    updated coupon_dao/ FindByDateAndID

--- dao/coupons_dao.go
+++ dao/coupons_dao.go
@@ -1,6 +1,8 @@
 package dao
 
 import (
+	"time"
+
 	. "github.com/ankit16-19/rasoi/dbConnection"
 	. "github.com/ankit16-19/rasoi/models"
 	mgo "gopkg.in/mgo.v2"
@@ -29,7 +31,11 @@ func (c *CouponDAO) FindByUserID(id string) ([]Coupon, error) {
 // FindByDateAndID :
 func (c *CouponDAO) FindByDateAndID(id string, date string) (Coupon, error) {
 	var coupon Coupon
-	err := Db.C(c.Collection).Find(bson.M{"userid": id, "weekstartdate": date}).One(&coupon)
+	t, err2 := time.Parse("2006-01-02", date)
+	if err2 != nil {
+		return coupon, err2
+	}
+	err := Db.C(c.Collection).Find(bson.M{"userid": id, "weekstartdate": t}).One(&coupon)
 	return coupon, err
 }
 
